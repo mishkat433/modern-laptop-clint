b2f39486155f7e03dfc0ff7f20536f6eba9afaa8
miskat433 <miskat433@gmail.com>
2022-11-25 01:36:12 +0600

    private route added

---
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from '../../Componemts/Spinner';
+import { AuthContex } from '../../Contex/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { loginUser, loading } = useContext(AuthContex);
+    const location = useLocation()
+    if (loading) {
+        return <Spinner />
+    }
+    if (loginUser?.uid) {
+        return children
+    }
+    return <Navigate to='/login' state={{ from: location }} replace />
+};
+
+export default PrivateRoute;
